278cff17788dfd5d28b4dcdfca7e51ad9e4c555f
Gökay YENER <gokay.yener@ibb.gov.tr>
2025-08-05 15:17:01 +0300

    ilk yukleme

---
+++ KFTSEntityLayer/Concrete/Gelir.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFTSEntityLayer.Concrete
+{
+    public class Gelir
+    {
+        [Key]
+        public int GelirId { get; set; }
+        public  MyProperty { get; set; }
+    }
+}
---
+++ KFTSEntityLayer/Concrete/Kullanıcı.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KFTSEntityLayer.Concrete
+{
+    public class Kullanıcı
+    {
+        [Key]
+        public int KullaniciId { get; set; }
+        public string KullaniciAdiSoyadi { get; set; }
+        public string KullaniciEmail { get; set; }
+        public string KullaniciSifre { get; set; }
+        public DateTime KullaniciOlusturulmaTarihi { get; set; }
+
+    }
+}
