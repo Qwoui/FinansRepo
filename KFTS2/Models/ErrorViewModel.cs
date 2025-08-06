b815f01e06afaca610d2886899b876ccfe9b4098
Gökay YENER <gokay.yener@ibb.gov.tr>
2025-08-06 15:43:06 +0300

    deneme

--- KFTS2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KFTS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
